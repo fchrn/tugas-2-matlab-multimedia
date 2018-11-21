f8d9e7691996dfc5184f09d929bb679ad30e27d8
fchrn <45229501+fchrn@users.noreply.github.com>
2018-11-21 18:12:00 +0700

    Add files via upload

---
+++ nomor1_rian.m
@@ -0,0 +1,18 @@
+A = [2 7; 5 3]
+B = [2 2; 5 6]
+
+tambah = A + B
+kurang = A - B
+bagi = A / B
+pangkat = A * A
+perkalian_antar_matriks = A * B
+perkalian_antar_elemen = A.*B
+perkalian_skalar = A*12
+
+disp(tambah);
+disp(kurang);
+disp(bagi);
+disp(pangkat);
+disp(perkalian_antar_matriks);
+disp(perkalian_antar_elemen);
+disp(perkalian_skalar);
---
+++ nomor2_Rian.m
@@ -0,0 +1,10 @@
+a = input('masukkan bilangan A: ');
+b = input('masukkan bilangan B: ');
+
+if a > b
+    hasil ='A lebih besar dari B';
+else 
+    hasil ='A tidak lebih besar dari B';
+end
+
+disp(hasil);
